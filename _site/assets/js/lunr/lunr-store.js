e334be00ffa256cd4fd3e1f27d2ac271c3aade82
sg3390 <sg3390@barnard.edu>
2023-02-26 21:46:01 -0500

    added updates

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,25 @@
-var store = []
+var store = [{
+        "title": "Project Proposal",
+        "excerpt":"Project Proposal This project will focus on building a tool that compares the performance of different e-commerce websites. More than ever, people have been shopping online to get whatever they need delivered to their doorstep without stepping out of their apartments. As shopping online became the habit for many people,...","categories": ["Proposal"],
+        "tags": [],
+        "url": "/website-sg3390/proposal/proposal/",
+        "teaser": null
+      },{
+        "title": "Update 1",
+        "excerpt":"Update Week 2 Tools to be used for the project As mentioned in the proposal, this project will utilize web scraping tools to obtain data from websites. Web scraping refers to the automated process of extracting data from website using software tools. For the scope of this project I will...","categories": [],
+        "tags": [],
+        "url": "/website-sg3390/update-1/",
+        "teaser": null
+      },{
+        "title": "Update 2",
+        "excerpt":"Update Week 3 Formatting the data Tools used for formatting the data: pandas DataFrame To store the data in an efficient way and easily visualize it, I utilized the pandas package for python. However, before storing the data in a dataframe format, I had to convert the dates of the...","categories": [],
+        "tags": [],
+        "url": "/website-sg3390/update-2/",
+        "teaser": null
+      },{
+        "title": "Update 3",
+        "excerpt":"Update Week 4 Identifying and scraping competitor products After meeting with my superviser, we have decided to add two other products from the same company to our analysis. These products are manufactured by Turkey’s third biggest food processing company Ulker. However, the products manufactured by Ulker are sold by other...","categories": [],
+        "tags": [],
+        "url": "/website-sg3390/update-3/",
+        "teaser": null
+      }]
